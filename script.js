893215c9975d506b5e6d491932e783abfb4c38dd
pabloesteves91 <fabioberta@me.com>
2024-12-21 15:22:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,35 +17,6 @@ function checkPassword() {
     }
 }
 
-// Admin-Panel Passwortschutz
-function showAdminPanel() {
-    const adminPassword = "swissportadmin";
-    const inputPassword = prompt("Bitte Admin-Passwort eingeben:");
-    if (inputPassword === adminPassword) {
-        document.getElementById("adminPanel").style.display = "block";
-    } else {
-        alert("Falsches Passwort!");
-    }
-}
-
-// Mitarbeiter hinzufügen
-document.getElementById("addEmployeeForm").addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const newEmployee = {
-        personalCode: document.getElementById("personalCode").value,
-        firstName: document.getElementById("firstName").value,
-        lastName: document.getElementById("lastName").value,
-        shortCode: document.getElementById("shortCode").value || null,
-        position: document.getElementById("position").value,
-        photo: `Fotos/${document.getElementById("firstName").value}_${document.getElementById("lastName").value}.jpg`
-    };
-
-    people.push(newEmployee);
-    alert("Mitarbeiter erfolgreich hinzugefügt!");
-    document.getElementById("addEmployeeForm").reset();
-});
-
 // Excel-Daten laden
 function loadExcelData() {
     const excelFilePath = "./Mitarbeiter.xlsx";
