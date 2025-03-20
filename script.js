426e10027be2f5641373434cb4c290076c25977c
pabloesteves91 <fabioberta@me.com>
2025-03-20 23:46:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -95,11 +95,13 @@ function searchEmployees() {
         const card = document.createElement("div");
         card.className = "result-card";
         card.innerHTML = `
-            <img src="${person.photo}" alt="${person.firstName}" class="clickable-img" width="100" height="100" onerror="this.src='Fotos/default.JPG';" onclick="openImageModal('${person.photo}')">
-            <h2>${person.firstName} ${person.lastName}</h2>
-            <p><span>Personalnummer:</span> ${person.personalCode}</p>
-            ${person.shortCode ? `<p><span>Kürzel:</span> ${person.shortCode}</p>` : ""}
-            <p><span>Position:</span> ${person.position}</p>
+            <img src="${person.photo}" alt="${person.firstName}" class="clickable-img" width="80" height="80" onerror="this.src='Fotos/default.JPG';">
+            <div class="result-info">
+                <h2>${person.firstName} ${person.lastName}</h2>
+                <p><span>Personalnummer:</span> ${person.personalCode}</p>
+                ${person.shortCode ? `<p><span>Kürzel:</span> ${person.shortCode}</p>` : ""}
+                <p class="position">${person.position}</p>
+            </div>
         `;
         results.appendChild(card);
     });
